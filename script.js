5f7c098f65f139fd69b5d9dbf97982e6da6e5e3b
Vedant08-blip <vedanttrivedi87@gmail.com>
2025-06-25 08:56:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, index) => {
+  input.addEventListener('input', () => {
+    if (input.value.length > 0 && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (input.value === '' && index > 0) {
+        inputs[index - 1].focus();
+        inputs[index - 1].value = '';
+        e.preventDefault();
+      } else {
+        input.value = '';
+      }
+    }
+  });
+});
+
+// Autofocus the first field on page load
+window.addEventListener('load', () => {
+  inputs[0].focus();
+});
